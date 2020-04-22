980049ab095413acfa44ef3416813ec874bfc7fc
JonathanQuerdenker <mr.querdenker@gmail.com>
2020-04-22 14:45:49 +0200

    Move middleware to middleware.js

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-const express = require('express')
 const http = require('http')
-const path = require('path')
 const socketIO = require('socket.io')
 require ('dotenv').config()
 
-const router = require("./server/router")
-
+// const express = require('express')
+// const path = require('path')
+// const router = require("./server/router")
 const port = 4001
-const app = express()
+const {app} = require('./server/middleware')
 
 
 if (process.env.NODE_ENV !== "production") {
@@ -19,25 +18,26 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 // our localhost port
-app.use(express.static("./public"))
+// app.use(express.static("./public")) //move to middleware.js
 // our server instance
 const server = http.createServer(app)
 
 // This creates our socket using the instance of the server
-const io = socketIO(server)
+const io = socketIO(server)//.listen(app.listen(4001))
 
+io.use((socket, next)=>middleware(socket.request, socket.request.res, next))
 //TODO serve middleware to io.server
 
 // This is what the socket.io syntax is like, we will work this later
 io.on('connection', socket => {
-  console.log('User connected')
+  console.log(`User with the id ${socket.id} connected`)
 
   socket.on('disconnect', () => {
-    console.log('user disconnected')
+    console.log('User disconnected')
   })
 })
 
-app.use('/', router)
+// app.use('/', router) //move to middleware.js
 
 
 server.listen(port, () => console.log(`Listening on port ${port}`))
--- server/middleware.js
+++ server/middleware.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const app = express()
-require('dotenv').config()
-const middleware = require('webpack-dev-middleware')
-
 const compression = require('compression')
-const cors = require('cors')
 const cookieSession = require('cookie-session')
 const cookieParser = require('cookie-parser')
-
 const csurf = require('csurf')
+const middleware = require('webpack-dev-middleware')
+const router = require("./router")
+require('dotenv').config()
+
 
 //QUESTION:  use req.session instead of cookie?
 
@@ -16,23 +15,26 @@ const webpack = require('webpack')
 const config = require('../webpack.config')
 const compiler = webpack(config)
 
-app.use(middleware(compiler, {}))
-    .use(cors())
-    .use(cookieParser())
-    .use(compression())
-    .use(csurf())
-    .use(function (req, res, next) {
-        res.cookie('myToken', req.csrfToken())
-        next()
-    })
-
 const cookieSessionMiddleware = cookieSession({
         name: "session",
-        secret: process.env.SESS_SECRET,
+        secret: process.env.SESSION_SECRET,
         maxAge: 1000 * 60 * 60 * 24
     }
 )
 
-app.use(cookieSessionMiddleware)
+app.use(middleware(compiler, {}))
+    .use(cookieParser())
+    .use(compression())
+    .use(csurf({cookie: true}))
+    // .use(function (req, res, next) {
+    //     res.cookie('securityToken', req.csrfToken())
+    //     next()
+    //
+    // })
+    .use(express.static("./public"))
+    .use('/', router)
+    .use(cookieSessionMiddleware)
+
+exports.app = app
 
 
